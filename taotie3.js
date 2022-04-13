a2cd2f672acef024a469f4ddc09687aab53d23c1
hyh315 <2856400640@qq.com>
2022-04-14 00:29:49 +0800

    taotiedoudou

--- taotie3.js
+++ taotie3.js
@@ -1,5 +1,9 @@
 $(function() {
     $("#dou").draggable({ containment: "#wall", scroll: false, cursorAt: { top: 25, left: 0 }, revert: "invalid" });
+    $("#dou2").draggable({ containment: "#wall", scroll: false, cursorAt: { top: 25, left: 0 }, revert: "invalid" });
+    $("#dou3").draggable({ containment: "#wall", scroll: false, cursorAt: { top: 25, left: 0 }, revert: "invalid" });
+    $("#dou4").draggable({ containment: "#wall", scroll: false, cursorAt: { top: 25, left: 0 }, revert: "invalid" });
+    $("#dou5").draggable({ containment: "#wall", scroll: false, cursorAt: { top: 25, left: 0 }, revert: "invalid" });
 
     $("#taotie").droppable({
         drop: function(event, ui) {
