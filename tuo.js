432eb8b9a5b8d30f4796f1cba2685b1ea6ea56d1
hyh315 <2856400640@qq.com>
2022-04-14 07:30:58 +0800

    tuotuo

--- tuo.js
+++ tuo.js
@@ -13,3 +13,28 @@ $(function() {
         }
     });
 });
+
+var touchValue = { x: 5, y: 5, sx: 0, sy: 0, ex: 0, ey: 0 }; //initialize the touch values
+window.addEventListener("touchstart", function () {
+    console.log("touchstart")
+    var event = event || window.event;
+    touchValue.sx = event.targetTouches[0].pageX;
+    touchValue.sy = event.targetTouches[0].pageY;
+    touchValue.ex = touchValue.sx;
+    touchValue.ey = touchValue.sy;
+});
+window.addEventListener("touchmove", function (event) {
+    var event = event || window.event;
+    event.preventDefault();
+    touchValue.ex = event.targetTouches[0].pageX;
+    touchValue.ey = event.targetTouches[0].pageY;
+    console.log("touchmove")
+});
+window.addEventListener("touchend", function (event) {
+    var event = event || window.event;
+    var changeX = touchValue.ex - touchValue.sx;
+    var changeY = touchValue.ey - touchValue.sy;
+    //console.log("X:"+changeX+" Y:"+changeY);
+    window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+    console.log("touchend")
+});
