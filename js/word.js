4cc17136054e427bff317f034e91aff3c11b7b0a
hyh315 <2856400640@qq.com>
2022-04-26 22:22:10 +0800

    tittle

---
+++ js/word.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#word1").hide();
+    $("#word2").hide();
+    $("#word3").hide();
+    $("#word4").hide();
+    $("#word1").fadeIn(3000);
+    $("#word2").fadeIn(4000);
+    $("#word3").fadeIn(5000);
+    $("#word4").fadeIn(6000);
+});
