c256591a8f1bdce1fab4126ed4afb4ff309a0f97
hyh315 <2856400640@qq.com>
2022-04-27 00:36:37 +0800

    word2

--- js/word.js
+++ js/word.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     $("#word2").hide();
     $("#word3").hide();
     $("#word4").hide();
-    $("#word1").fadeIn(3000);
-    $("#word2").fadeIn(4000);
-    $("#word3").fadeIn(5000);
-    $("#word4").fadeIn(6000);
+    $("#word1").fadeIn(1000);
+    $("#word2").fadeIn(1500);
+    $("#word3").fadeIn(2000);
+    $("#word4").fadeIn(5000);
 });
